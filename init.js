ac818342c706fc92a1312e02fc0de0898bfe90ba
Sean Goresht <seangores@gmail.com>
2016-09-06 22:28:59 -0600

    Reformat init questions

--- init.js
+++ init.js
@@ -1,21 +1,26 @@
 exports.configure = [
-  {
-    name: 'name',
-    message: 'What is the name of your project?'
-  }, {
-    name: 'description',
-    message: 'Describe your project'
-  }, {
-    name: 'github_username',
-    message: 'What is your github username?'
-  }, {
-    name: 'production',
-    message: 'Would you like a production config file?',
-    type: 'confirm',
-    default: true
-  }
+	{
+		name: "name",
+		message: "What is the name of your project?"
+	},
+	{
+		name: "description",
+		message: "Describe your project"
+	},
+	{
+		name: "github_username",
+		message: "What is your github username?"
+	},
+	{
+		name: "production",
+		message: "Would you like a production config file?",
+		type: "confirm",
+		default: true
+	}
 ]
 
 exports.after = (util, config) => {
-  if (!config.production) util.target.remove('app.production.js')
+	if (!config.production) {
+		util.target.remove("app.production.js")
+	}
 }
