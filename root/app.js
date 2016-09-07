51f685b4dbd7099717b3e8b987dfca5a9fb6350f
Sean Goresht <seangores@gmail.com>
2016-09-06 22:28:19 -0600

    Reformat environment configs

--- root/app.js
+++ root/app.js
@@ -1,23 +1,23 @@
-const htmlStandards = require('reshape-standard')
-const cssStandards = require('spike-css-standards')
-const jsStandards = require('babel-preset-latest')
-const pageId = require('spike-page-id')
+const htmlStandards = require("reshape-standard")
+const cssStandards = require("spike-css-standards")
+const jsStandards = require("babel-preset-latest")
+const pageId = require("spike-page-id")
 
 module.exports = {
-  devtool: 'source-map',
-  matchers: {
-    html: '*(**/)*.sgr',
-    css: '*(**/)*.sss'
-  },
-  ignore: ['**/layout.sgr', '**/_*', '**/.*'],
-  reshape: (ctx) => {
-    return htmlStandards({
-      webpack: ctx,
-      locals: { pageId: pageId(ctx), foo: 'bar' }
-    })
-  },
-  postcss: (ctx) => {
-    return cssStandards({ webpack: ctx })
-  },
-  babel: { presets: [jsStandards] }
+	devtool: "source-map",
+	matchers: {
+		html: "*(**/)*.sgr",
+		css: "*(**/)*.sss"
+	},
+	ignore: ["**/layout.sgr", "**/_*", "**/.*"],
+	reshape: (ctx) => {
+		return htmlStandards({
+			webpack: ctx,
+			locals: {pageId: pageId(ctx), foo: "bar"}
+		})
+	},
+	postcss: (ctx) => {
+		return cssStandards({webpack: ctx})
+	},
+	babel: {presets: [jsStandards]}
 }
